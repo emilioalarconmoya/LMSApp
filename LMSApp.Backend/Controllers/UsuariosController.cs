8c2dcdce63cb88d81a19a97b52e919f70af73825
emilioalarconmoya <daniel.mat@gmail.com>
2021-09-01 18:10:45 -0400

    Consultar con stored procedure

---
+++ LMSApp.Backend/Controllers/UsuariosController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LMSApp.Backend.Models;
+using LMSApp.Common.Models;
+using System.Data.SqlClient;
+
+namespace LMSApp.Backend.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private LocalDataContext db = new LocalDataContext();
+
+        // GET: Usuarios
+        public async Task<ActionResult> Index()
+        {
+
+            //return View(await db.Usuarios.ToListAsync());
+            return View(await db.Database.SqlQuery<Usuario>("exec proc_select_USUARIO_all_app @cod_empresa", new SqlParameter("@cod_empresa", 1)).ToListAsync());
+            //ViewResult aaa;
+            //aaa = View(db.Database.SqlQuery<Usuario>("exec proc_select_USUARIO_all @cod_empresa", new SqlParameter("@cod_empresa", 1)));
+            //return aaa;
+            //return await query.ToListAsync<ActionResult>();
+
+        }
+
+        // GET: Usuarios/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = await db.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "UsuarioId,RutUsuario,CodEmpresa,Nombres,Apellidos,Password,Email,FechaCaducidad,FechaCreacion,Bloqueado,ClaveSence,Profesion,Direccion,Comuna,Dni,Fono")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(usuario);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = await db.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "UsuarioId,RutUsuario,CodEmpresa,Nombres,Apellidos,Password,Email,FechaCaducidad,FechaCreacion,Bloqueado,ClaveSence,Profesion,Direccion,Comuna,Dni,Fono")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = await db.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Usuario usuario = await db.Usuarios.FindAsync(id);
+            db.Usuarios.Remove(usuario);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LMSApp.Common/Models/Usuario.cs
@@ -0,0 +1,44 @@
+﻿
+namespace LMSApp.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Usuario
+    {
+
+        [Key]
+        public int UsuarioId { get; set; }
+        [Required]
+        public int RutUsuario { get; set; }
+       
+        public int CodEmpresa { get; set; }
+       
+        public string Nombres { get; set; }
+       
+        public string Apellidos { get; set; }
+        
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime FechaCaducidad { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+       
+        public Boolean Bloqueado { get; set; }
+
+        public string ClaveSence { get; set; }
+
+        public string Profesion { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Comuna { get; set; }
+       
+        public string Dni { get; set; }
+
+        public string Fono { get; set; }
+    }
+
+}
