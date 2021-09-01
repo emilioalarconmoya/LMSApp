4853bcc8b45f4bdc8fd1530cc5940e20e593994a
emilioalarconmoya <daniel.mat@gmail.com>
2021-09-01 14:02:53 -0400

    CREACION DE PROYECTO

---
+++ LMSApp.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LMSApp.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ LMSAppBackend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMSAppBackend.Startup))]
+namespace LMSAppBackend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
